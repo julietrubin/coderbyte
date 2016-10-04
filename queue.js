5b6a5740f4fe4c3352c4c0a26cb7eac9d916d666
Juliet Rubin <jcrubin@usfca.edu>
2016-10-04 15:01:00 -0700

    adding files

--- queue.js
+++ queue.js
@@ -39,16 +39,9 @@ class Node {
 		this.next = null
 	}
 }
+
 q = new Queue()
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-console.log(q.dequeue())
-console.log(q.dequeue())
-q.enqueue(4)
-console.log(q.dequeue())
-console.log(q.dequeue())
-console.log(q.dequeue())
+
 
 
 
