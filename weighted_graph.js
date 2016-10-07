1a6b79afba95d61582eeb22f43bf6f78f4f7e5a9
Juliet Rubin <jcrubin@usfca.edu>
2016-10-07 10:46:46 -0700

    saved weighted_graph.js

--- weighted_graph.js
+++ weighted_graph.js
@@ -1,6 +1,14 @@
-Using the JavaScript language, have the function WeightedPath(strArr) take strArr which will be an array of strings which models a non-looping weighted Graph. The structure of the array will be as follows: The first element in the array will be the number of nodes N (points) in the array as a string. The next N elements will be the nodes which can be anything (A, B, C .. Brick Street, Main Street .. etc.). Then after the Nth element, the rest of the elements in the array will be the connections between all of the nodes along with their weights (integers) separated by the pipe symbol (|). They will look like this: (A|B|3, B|C|12 .. Brick Street|Main Street|14 .. etc.). Although, there may exist no connections at all. 
+/*Using the JavaScript language, have the function WeightedPath(strArr) take strArr which will be an array of strings which models a non-looping weighted Graph.
+ The structure of the array will be as follows: The first element in the array will be the number of nodes N (points) in the array as a string. The next N elements 
+will be the nodes which can be anything (A, B, C .. Brick Street, Main Street .. etc.). Then after the Nth element, the rest of the elements in the array will be 
+the connections between all of the nodes along with their weights (integers) separated by the pipe symbol (|). They will look like this: (A|B|3, B|C|12 .. 
+Brick Street|Main Street|14 .. etc.). Although, there may exist no connections at all. 
 
-An example of strArr may be: ["4","A","B","C","D","A|B|1","B|D|9","B|C|3","C|D|4"]. Your program should return the shortest path when the weights are added up from node to node from the first Node to the last Node in the array separated by dashes. So in the example above the output should be A-B-C-D. Here is another example with strArr being ["7","A","B","C","D","E","F","G","A|B|1","A|E|9","B|C|2","C|D|1","D|F|2","E|D|6","F|G|2"]. The output for this array should be A-B-C-D-F-G. There will only ever be one shortest path for the array. If no path between the first and last node exists, return -1. The array will at minimum have two nodes. Also, the connection A-B for example, means that A can get to B and B can get to A. A path may not go through any Node more than once. 
+An example of strArr may be: ["4","A","B","C","D","A|B|1","B|D|9","B|C|3","C|D|4"]. Your program should return the shortest path when the weights are added up 
+from node to node from the first Node to the last Node in the array separated by dashes. So in the example above the output should be A-B-C-D. Here is another 
+example with strArr being ["7","A","B","C","D","E","F","G","A|B|1","A|E|9","B|C|2","C|D|1","D|F|2","E|D|6","F|G|2"]. The output for this array should be A-B-C-D-F-G. 
+There will only ever be one shortest path for the array. If no path between the first and last node exists, return -1. The array will at minimum have two nodes. 
+Also, the connection A-B for example, means that A can get to B and B can get to A. A path may not go through any Node more than once. 
 
 Hard challenges are worth 15 points and you are not timed for them.
 Sample Test Cases
@@ -11,8 +19,7 @@ Input:"4","x","y","z","w","x|y|2","y|z|14", "z|y|31"
 Output:-1
 
 
-"6","A","B","C","D","E","F","B|A|2","A|F|12","A|C|4","B|D|1","D|E|1","C|D|4","F|E|1"
-
+*/
 
 function BuildGraph(strArr, numNodes){
     var graph = {};
